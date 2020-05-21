dbc87dd0385c95bad0ff0ca1b6f0fe4694f67742
Raphael Büchi <raphael.buechi@hotmail.com>
2020-05-21 23:29:51 +0200

    implementation

---
+++ src/main/java/ch/netider/AzureAdDeployer/service/exo/ExoService.java
@@ -0,0 +1,10 @@
+package ch.netider.AzureAdDeployer.service.exo;
+
+import ch.netider.AzureAdDeployer.session.PsSession;
+
+public class ExoService {
+
+    private final PsSession session = new PsSession("msolPsSession");
+
+
+}
