9546c79406268abe4b6d679748ada8650449f975
Raphael Büchi <raphael.buechi@hotmail.com>
2021-02-13 14:04:42 +0100

    Added Features

--- src/main/java/ch/netider/AzureAdDeployer/gui/controller/SettingsController.java
+++ src/main/java/ch/netider/AzureAdDeployer/gui/controller/SettingsController.java
@@ -4,6 +4,7 @@ import ch.netider.AzureAdDeployer.service.maintenance.MaintenanceService;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextArea;
 
 public class SettingsController {
@@ -11,23 +12,31 @@ public class SettingsController {
     @FXML
     TextArea mainOutput;
 
+    @FXML
+    ProgressIndicator progress;
+
     private MaintenanceService maintenanceService;
 
-    public SettingsController(){
+    public SettingsController() {
         maintenanceService = new MaintenanceService();
     }
 
-    public void showInstalledModulesButton(ActionEvent event){
+    public void showInstalledModulesButton(ActionEvent event) {
         Task<String> task = new Task<>() {
             @Override
             protected String call() {
+                progress.setStyle("visibility: visible");
                 return maintenanceService.showModules();
             }
+
             @Override
             protected void succeeded() {
                 mainOutput.setText(call());
+                updateProgress(1, 1);
+                progress.setStyle("visibility: hidden");
             }
         };
+        progress.progressProperty().bind(task.progressProperty());
         new Thread(task).start();
     }
 
@@ -36,14 +45,19 @@ public class SettingsController {
         Task<String> task = new Task<>() {
             @Override
             protected String call() {
+                progress.setStyle("visibility: visible");
                 maintenanceService.installMsol();
                 return maintenanceService.showModules();
             }
+
             @Override
             protected void succeeded() {
                 mainOutput.setText(call());
+                updateProgress(1, 1);
+                progress.setStyle("visibility: hidden");
             }
         };
+        progress.progressProperty().bind(task.progressProperty());
         new Thread(task).start();
     }
 
@@ -51,14 +65,19 @@ public class SettingsController {
         Task<String> task = new Task<>() {
             @Override
             protected String call() {
+                progress.setStyle("visibility: visible");
                 maintenanceService.uninstallMsol();
                 return maintenanceService.showModules();
             }
+
             @Override
             protected void succeeded() {
                 mainOutput.setText(call());
+                updateProgress(1, 1);
+                progress.setStyle("visibility: hidden");
             }
         };
+        progress.progressProperty().bind(task.progressProperty());
         new Thread(task).start();
     }
 }
--- src/main/java/ch/netider/AzureAdDeployer/gui/controller/UserManagerLegacyController.java
+++ src/main/java/ch/netider/AzureAdDeployer/gui/controller/UserManagerLegacyController.java
@@ -4,6 +4,7 @@ import ch.netider.AzureAdDeployer.service.msol.MsolService;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextArea;
 
 public class UserManagerLegacyController {
@@ -11,47 +12,149 @@ public class UserManagerLegacyController {
     @FXML
     TextArea mainOutput;
 
+    @FXML
+    ProgressIndicator progress;
+
     private MsolService msolService;
 
     public UserManagerLegacyController() {
         msolService = new MsolService();
     }
 
-    public void showAllUsersButton(ActionEvent event){
+    public void showAllUsersButton(ActionEvent event) {
         Task<String> task = new Task<>() {
             @Override
             protected String call() {
+                progress.setStyle("visibility: visible");
                 return msolService.showAllUsers();
             }
+
             @Override
             protected void succeeded() {
                 mainOutput.setText(call());
+                updateProgress(1, 1);
+                progress.setStyle("visibility: hidden");
             }
         };
+        progress.progressProperty().bind(task.progressProperty());
         new Thread(task).start();
     }
 
-    public void createBgAccountsButton(ActionEvent event){
+    public void createBgAccountsButton(ActionEvent event) {
+        Task<String> task = new Task<>() {
+            @Override
+            protected String call() {
+                progress.setStyle("visibility: visible");
+                msolService.createBreakGlassAccounts();
+                return msolService.showBreakGlassAccounts();
+            }
 
+            @Override
+            protected void succeeded() {
+                mainOutput.setText(call());
+                updateProgress(1, 1);
+                progress.setStyle("visibility: hidden");
+            }
+        };
+        progress.progressProperty().bind(task.progressProperty());
+        new Thread(task).start();
     }
 
-    public void showBgAccountsButton(ActionEvent event){
+    public void showBgAccountsButton(ActionEvent event) {
+        Task<String> task = new Task<>() {
+            @Override
+            protected String call() {
+                progress.setStyle("visibility: visible");
+                return msolService.showBreakGlassAccounts();
+            }
 
+            @Override
+            protected void succeeded() {
+                mainOutput.setText(call());
+                updateProgress(1, 1);
+                progress.setStyle("visibility: hidden");
+            }
+        };
+        progress.progressProperty().bind(task.progressProperty());
+        new Thread(task).start();
     }
 
-    public void removeBgAccountsButton(ActionEvent event){
+    public void removeBgAccountsButton(ActionEvent event) {
+        Task<String> task = new Task<>() {
+            @Override
+            protected String call() {
+                progress.setStyle("visibility: visible");
+                msolService.removeBreakGlassAccounts();
+                return msolService.showBreakGlassAccounts();
+            }
 
+            @Override
+            protected void succeeded() {
+                mainOutput.setText(call());
+                updateProgress(1, 1);
+                progress.setStyle("visibility: hidden");
+            }
+        };
+        progress.progressProperty().bind(task.progressProperty());
+        new Thread(task).start();
     }
 
-    public void checkMfaStatusButton(ActionEvent event){
+    public void checkMfaStatusButton(ActionEvent event) {
+        Task<String> task = new Task<>() {
+            @Override
+            protected String call() {
+                progress.setStyle("visibility: visible");
+                return msolService.checkMfa();
+            }
 
+            @Override
+            protected void succeeded() {
+                mainOutput.setText(call());
+                updateProgress(1, 1);
+                progress.setStyle("visibility: hidden");
+            }
+        };
+        progress.progressProperty().bind(task.progressProperty());
+        new Thread(task).start();
     }
 
-    public void enableMfaForAllButton(ActionEvent event){
+    public void enableMfaForAllButton(ActionEvent event) {
+        Task<String> task = new Task<>() {
+            @Override
+            protected String call() {
+                progress.setStyle("visibility: visible");
+                msolService.enableMfa("raphael.buechi@netider.ch");
+                return msolService.checkMfa();
+            }
 
+            @Override
+            protected void succeeded() {
+                mainOutput.setText(call());
+                updateProgress(1, 1);
+                progress.setStyle("visibility: hidden");
+            }
+        };
+        progress.progressProperty().bind(task.progressProperty());
+        new Thread(task).start();
     }
 
-    public void disableMfaForAllButton(ActionEvent event){
+    public void disableMfaForAllButton(ActionEvent event) {
+        Task<String> task = new Task<>() {
+            @Override
+            protected String call() {
+                progress.setStyle("visibility: visible");
+                msolService.disableMfa("raphael.buechi@netider.ch");
+                return msolService.checkMfa();
+            }
 
+            @Override
+            protected void succeeded() {
+                mainOutput.setText(call());
+                updateProgress(1, 1);
+                progress.setStyle("visibility: hidden");
+            }
+        };
+        progress.progressProperty().bind(task.progressProperty());
+        new Thread(task).start();
     }
 }
